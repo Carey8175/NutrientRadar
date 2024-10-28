f49bbb635f456101f74a602092b5a6fd8c27ce9e
carey <980328708@qq.com>
2024-10-28 20:05:28 +0800

    v1.4.6 add LLMs configs

--- SystemCode/configs/basic.py
+++ SystemCode/configs/basic.py
@@ -15,3 +15,9 @@ FOOD_CONFIDENCE_THRESHOLD = 0.5
 FOOD_NUTRITION_CSV_PATH = os.path.join(ROOT_PATH, 'sources', 'nutrition.csv')
 
 CREDIT_CARD_AREA = 46.2     # cm^2
+
+USE_QWEN = False
+QWEN_MODEL_NAME = 'Qwen/Qwen2.5-1.5B-Instruct'
+API_KEY = 'sk-12bb34c4ad424f3d818f67ed1d137910'
+BASE_URL = 'https://api.deepseek.com'
+API_MODEL_NAME = 'deepseek-chat'
